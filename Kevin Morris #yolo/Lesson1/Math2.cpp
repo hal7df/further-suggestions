88441b798456ddb8fc7424f07913e6f553d752e9
msubde21 <msubde21@gmail.com>
2014-01-11 13:18:01 -0500

    Created folder "Kevin Morris" For C++ tutorial code

---
+++ Kevin Morris #yolo/Lesson1/Math2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b,c;
+    float result1 = a+c;
+    float result2 = result1/b;
+
+    cout <<"Enter three numbers ";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout <<"The sum of the first and third number is: " << result1;\n
+    cout <<"That number divided by the second number is: " << result2;\n
+    return 0;
+}
