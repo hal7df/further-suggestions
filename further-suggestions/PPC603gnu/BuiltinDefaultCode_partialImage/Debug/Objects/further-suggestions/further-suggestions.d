ebf91e0bf0a5651f5ddceb1da70f72c80a865580
jyori112 <jyori112@gmail.com>
2014-02-08 18:21:51 -0500

    adding joystick wrapper for future use

--- further-suggestions/PPC603gnu/BuiltinDefaultCode_partialImage/Debug/Objects/further-suggestions/further-suggestions.d
+++ further-suggestions/PPC603gnu/BuiltinDefaultCode_partialImage/Debug/Objects/further-suggestions/further-suggestions.d
@@ -384,6 +384,7 @@
   C:/WindRiver/vxworks-6.3/target/h/WPILib/Vision/HSLImage.h \
   C:/WindRiver/vxworks-6.3/target/h/WPILib/Vision/ColorImage.h \
   C:/WindRiver/vxworks-6.3/target/h/WPILib/WPIErrors.h \
+  C:/WindRiver/workspace/further-suggestions/further-suggestions/JoystickWrapper.h \
   C:/WindRiver/workspace/further-suggestions/further-suggestions/DriveWrapper.h \
   C:/WindRiver/workspace/further-suggestions/further-suggestions/Defines.h \
   c:/windriver/gnu/3.4.4-vxworks-6.3/x86-win32/lib/gcc/../../../x86-win32/include/c++/3.4.4/cmath \
@@ -1154,6 +1155,8 @@ C:/WindRiver/vxworks-6.3/target/h/WPILib/Vision/HSLImage.h:
 C:/WindRiver/vxworks-6.3/target/h/WPILib/Vision/ColorImage.h:
 
 C:/WindRiver/vxworks-6.3/target/h/WPILib/WPIErrors.h:
+
+C:/WindRiver/workspace/further-suggestions/further-suggestions/JoystickWrapper.h:
 
 C:/WindRiver/workspace/further-suggestions/further-suggestions/DriveWrapper.h:
 
